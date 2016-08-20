4df554363f05fb18b0ccada8fb520ceaa582ed8e
angcyo <angcyo@126.com>
2016-08-20 15:39:32 +0800

    add playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,47 @@
+// : Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+let nsFileM = NSFileManager.defaultManager()
+
+let nsFileM3 = NSFileManager.defaultManager()
+
+nsFileM == nsFileM3
+
+let nsFileM2 = NSFileManager()
+let nsFileM4 = NSFileManager()
+
+nsFileM2 == nsFileM4
+
+nsFileM == nsFileM2
+
+nsFileM.displayNameAtPath("/asdf/asdfasdf/asdgasdg.name/")
+try? nsFileM.attributesOfFileSystemForPath("/")
+
+let fManager = nsFileM
+
+fManager.URLsForDirectory(.AllApplicationsDirectory, inDomains: .UserDomainMask)
+fManager.URLsForDirectory(.AllApplicationsDirectory, inDomains: .AllDomainsMask)
+
+fManager.URLsForDirectory(.DocumentationDirectory, inDomains: .UserDomainMask)
+fManager.URLsForDirectory(.DocumentationDirectory, inDomains: .LocalDomainMask)
+fManager.URLsForDirectory(.DocumentationDirectory, inDomains: .NetworkDomainMask)
+fManager.URLsForDirectory(.DocumentationDirectory, inDomains: .SystemDomainMask)
+
+fManager.URLsForDirectory(.DocumentationDirectory, inDomains: .AllDomainsMask)
+
+fManager.URLsForDirectory(.DownloadsDirectory, inDomains: .AllDomainsMask)
+
+fManager.URLsForDirectory(.ItemReplacementDirectory, inDomains: .AllDomainsMask)
+fManager.URLsForDirectory(.CachesDirectory, inDomains: .AllDomainsMask)
+
+let docDirectory = fManager.URLsForDirectory(.DocumentationDirectory, inDomains: .UserDomainMask).first!
+
+let ArchiveURL = docDirectory.URLByAppendingPathComponent("datas")
+
+
+
+
+
--- TableViewDemo/DataBean.swift
+++ TableViewDemo/DataBean.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class DataBean: CustomStringConvertible {
+class DataBean: NSObject, NSCoding {
 	var image: UIImage?
 	var name: String
 	var rate: Int
-	init(name: String, rate: Int, image: UIImage?) {
+	init?(name: String, rate: Int, image: UIImage?) {
 		self.image = image
 		self.rate = rate
 		self.name = name
+
+		super.init()
+
+		if name.isEmpty || rate < 0 {
+			return nil
+		}
 	}
 
-	var description: String {
+	override var description: String {
 		return "image:\(image) name:\(name) rate:\(rate)"
 	}
+
+	required convenience init?(coder aDecoder: NSCoder) {
+		let image = aDecoder.decodeObjectForKey(DataKey.imageKey) as! UIImage
+		let name = aDecoder.decodeObjectForKey(DataKey.nameKey) as! String
+		let rate = aDecoder.decodeIntegerForKey(DataKey.rateKey)
+
+		self.init(name: name, rate: rate, image: image)
+	}
+
+	func encodeWithCoder(aCoder: NSCoder) {
+		aCoder.encodeObject(image, forKey: DataKey.imageKey)
+		aCoder.encodeObject(name, forKey: DataKey.nameKey)
+		aCoder.encodeInteger(rate, forKey: DataKey.rateKey)
+	}
+}
+
+struct DataKey {
+	static let imageKey = "image"
+	static let nameKey = "name"
+	static let rateKey = "rate"
 }
